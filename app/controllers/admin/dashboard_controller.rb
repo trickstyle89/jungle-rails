54e629f1d1fe7daace0232aefd6dbd762b21fce6
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-04-05 23:13:26 -0300

    Completed Stretch

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
 
   def show
-    
+    @product_count = Product.count
+    @category_count = Category.count
   end
+
 end
