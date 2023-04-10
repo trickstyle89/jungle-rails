57707c8469747bf2e3c7db8238c2d73488301428
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-04-10 18:00:46 -0300

    Fixed the email cehck and also tested with RSpec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,8 @@ def create
     session[:user_id] = @user.id
     redirect_to '/'
   else
-    if User.find_by(email: @user.email.downcase)
-      flash[:error] = 'Email has already been taken.'
-    end
-    redirect_to '/signup'
+    flash[:error] = 'Email has already been taken.'
+    render 'new'
   end
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 
+    validates :email, presence: true, uniqueness: { case_sensitive: false }
+
     has_secure_password
 
 end
